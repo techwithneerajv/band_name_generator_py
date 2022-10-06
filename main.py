d542f897b6caa8dfe500a6dc5b602d94aeb94747
techwithneerajv <115125835+techwithneerajv@users.noreply.github.com>
2022-10-06 21:33:31 +0530

    Main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hi there! Are you confused with ur band name? We are here to help")
+
+Name1 = input("Enter your city you grew up in: \n ")
+
+Name2 = input("Enter your pet's name: \n ")
+
+final = Name1 + " "+Name2
+
+print("Congratulations! Your new band name is "+ final)
